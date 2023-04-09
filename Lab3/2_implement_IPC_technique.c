7daf1050416d25ab46f877726a434ad69556ec4a
subodhstha <shresthasubodh73@gmail.com>
2023-04-09 22:17:07 +0545

    lab 3

---
+++ Lab3/2_implement_IPC_technique.c
@@ -0,0 +1,40 @@
+//Implement IPC Technique
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<stdio.h>
+void *thread1f(void *arg);
+void *thread2f(void *arg);
+int turn = 1;
+int main()
+{
+	pthread_t thid1;
+	pthread_t thid2;
+	pthread_create(&thid1, NULL, &thread1f, NULL);
+	pthread_create(&thid2, NULL, &thread2f, NULL);
+	pthread_join(thid1, NULL);
+	pthread_join(thid2, NULL);
+	return 0;
+}
+
+void *thread1f(void *arg)
+{
+	int a = 0;
+	while(a++ < 20)
+	{
+		while(turn != 1);
+		fputc('b',stderr);
+		turn = 0;
+	}
+}
+
+void *thread2f(void *arg)
+{
+	int b = 0;
+	while(b++ < 20)
+	{
+		while(turn != 0);
+		fputc('a', stderr);
+		turn = 1;
+	}
+}
---
+++ Lab3/3_simulating_FCFS_FIFO_scheduling.c
@@ -0,0 +1,36 @@
+//similating First Come First Serve(FCFS) FIFO scheduling in c.
+#include<stdio.h>
+int main()
+{
+	int n, bt[20], wt[20], tat[20], avwt = 0, avtat = 0, i, j;
+	printf("Enter total number of processes(maximum 20) ");
+	scanf("%d", &n);
+	scanf("Enter Process Burst Time \n");
+	for(i = 0; i < n; i++)
+	{
+		printf("P[%d] ",i+1);
+		scanf("%d",&bt[i]);
+	}
+	wt[0] = 0; //waiting time for first process is 0
+	for(i = 1; i < n; i++) //calculation waiting time
+	{
+		wt[i] = 0;
+		for(j = 0; j < i; j++)
+		{
+			wt[i] = wt[i] + bt[j];
+		}
+	}
+	printf("Process\t Burst Time\t Waiting Time\t Turnaround Time\n");
+	for(i = 0; i < n; i++)//calculating turnaround time
+	{
+		tat[i] = bt[i] + wt[i];
+		avwt += wt[i];
+		avtat += tat[i];
+		printf("P[%d]\t\t%d\t\t%d\t\t%d\n", i+1, bt[i], wt[i], tat[i]);
+	}
+	avwt /= i;
+	avtat /= i;
+	printf("Average Waiting Time %d\n", avwt);
+	printf("Average Turnaround Time %d\n", avtat);
+	return 0;
+}
